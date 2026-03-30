af72b4f5e61610bbdd4fa57069139cc0e94e89f7
Sriram Venkata Krishna <143096990+svkrish01@users.noreply.github.com>
2026-03-30 23:33:32 +0530

    Create 172. Waveform Analysis - Sequential Circuit 9.v

---
+++ 172. Waveform Analysis - Sequential Circuit 9.v
@@ -0,0 +1,32 @@
+/*
+Developer   - Sriram Venkata Krishna
+Date        - 30-03-2026
+Platform    - HDL Bits
+*/
+
+//172. Waveform Analysis - Sequential Circuit 9
+
+module top_module 
+    (
+        input clk,
+        input a,
+        output [3:0] q 
+    );
+    
+    always @(posedge clk) begin
+        if (a) begin
+            q <= 4'd4;
+        end
+        
+        else begin 
+            if(q[3:0] == 4'd6) begin
+                q <= 4'd0;
+            end
+            
+            else begin
+                q <= q + 1;
+            end
+        end
+    end          
+
+endmodule
